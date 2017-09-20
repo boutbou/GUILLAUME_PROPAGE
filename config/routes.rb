7728c5a71b9358028e6727b2ac24155b1fb08926
clement Bouttier <boubou.clem@gmail.com>
2017-09-20 18:08:51 +0200

    inital design ok , responsive seems ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/contact'
-
-  get 'pages/a_propos'
-
-  get 'pages/pathologies'
-
-  get 'pages/rendez_vous'
+  get 'pages/home'
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
