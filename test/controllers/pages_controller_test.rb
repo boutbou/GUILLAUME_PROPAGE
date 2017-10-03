07e6198f00e1da793055451c7cbbb3eff675d312
clement Bouttier <boubou.clem@gmail.com>
2017-10-03 16:30:43 +0200

    meta modifiées a verifier live, controllers et view créées

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get rdv" do
+    get pages_rdv_url
+    assert_response :success
+  end
+
+  test "should get soins" do
+    get pages_soins_url
+    assert_response :success
+  end
+
   test "should get home" do
     get pages_home_url
     assert_response :success
